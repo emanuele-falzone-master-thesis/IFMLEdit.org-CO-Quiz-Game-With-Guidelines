fc778ed6a3f1471890861fa188af27a1770a6cf0
Emanuele Falzone <emafalzo@gmail.com>
2018-08-28 13:54:07 +0200

    merged model
    almost-git

--- src/js/actions/action-update-level-2.js
+++ src/js/actions/action-update-level-2.js
@@ -11,25 +11,73 @@ function Action() { // add "options" parameters if needed
     */
 }
 Action.prototype.run = function (parameters, solve) { // add "onCancel" parameters if needed
-    // Parameters:
-    // parameters['correctness']
-    // parameters['id']
+    var level = localStorage.getItem("question.level"),
+        count = parseInt(localStorage.getItem("question.count"));
 
-    // TODO: Execution
-    /*
-    example:
-    mail.find({subject: 'Re: ' + data.subject})
-        .then(solve);
-    */
-    // THIS CAN BE REMOVED (BEGIN)
-    Materialize.toast('update level', 2000)
+    var count_back = -2;
+    var count_up_1_2 = 10;
+    var count_up_2_3 = 15;
+
+    switch (level) {
+        case "1":
+            if (parameters['correctness'] === true) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count >= count_up_1_2) {
+                count = 0;
+                level = "2";
+                Materialize.toast('Level 2', 2000);
+            }
+            break;
+        case "2":
+            if (parameters['correctness'] === true) {
+                if (count < 0) {
+                    count = 0;
+                }
+                count++;
+            } else {
+                if (count > 0) {
+                    count = 0;
+                }
+                count--;
+            }
+
+            if (count <= count_back) {
+                count = 0;
+                level = "1";
+                Materialize.toast('Level 1', 2000);
+            }
+            if (count >= count_up_2_3) {
+                count = 0;
+                level = "3";
+                Materialize.toast('Level 3', 2000);
+            }
+            break;
+        case "3":
+            if (parameters['correctness'] === true) {
+                count = 0;
+            } else {
+                count--;
+            }
+            if (count <= count_back) {
+                count = 0;
+                level = "2";
+                Materialize.toast('Level 2', 2000);
+            }
+            break;
+    }
+
+    localStorage.setItem("question.level", level);
+    localStorage.setItem("question.count", count);
+
     solve({
         event: 'event-update-level-2-done', // done
         data: {
             'id': parameters['id'],
         }
     });
-    // THIS CAN BE REMOVED (END)
 };
 
 exports.createAction = function (options) {
--- src/js/repositories/answers/index.js
+++ src/js/repositories/answers/index.js
@@ -9,29 +9,28 @@ function Repository(options) {
         return new Repository(options);
     }
 
-    // TODO: initialization
-
-    // TODO: remove this BEGIN
     this.db = Promise.promisifyAll(new DataStore({
         filename: 'answers',
         inMemoryOnly: true
     }));
-    this.db.insert(require('./mock'));
-    // TODO: remove this END
+
+    this.db.insert(require('./default'));
 }
 
 Repository.prototype.findById = function (id) {
-    // TODO: implement the accessor to the datasource which returns a promise
-    // TODO: remove this BEGIN
     return this.db.findOneAsync({ id: id });
-    // TODO: remove this END
 };
 
 Repository.prototype.find = function (fields, project) {
-    // TODO: implement the accessor to the datasource which returns a promise
-    // TODO: remove this BEGIN
     return this.db.findAsync(fields, project);
-    // TODO: remove this END
+};
+
+Repository.prototype.insert = function (fields) {
+    var self = this;
+    return this.db.removeAsync({ question: 'online' }, { multi: true })
+        .then(function () {
+            return self.db.insert(fields);
+        });
 };
 
 exports.createRepository = Repository;
